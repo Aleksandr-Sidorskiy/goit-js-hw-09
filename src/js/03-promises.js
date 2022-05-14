01b63772e6f9c4e715f6110c0fd0325d5b33f2f0
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-05-14 20:44:37 +0300

    task 2 and 3 HW

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formProm = document.querySelector('.form');
+formProm.addEventListener('submit', onFormSubmit)
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
+
+  for (let position = 1; position <= amount; position += 1){
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
+        }, delay)
+      })
+    delay += step;
+   }
 }
+
+
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    const ObjektPromise = { position, delay };
+
+    return new Promise((resolve, rejekt) => {
+
+      if (shouldResolve) {
+        resolve(ObjektPromise);
+      } 
+      rejekt(ObjektPromise);
+    })
+  }
